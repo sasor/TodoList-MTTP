c32493bfaba0b9f874f824c36fd5a41c421c4cb3
sasor <s.leamsi99@gmail.com>
2016-12-18 10:00:39 -0500

    [U] ruta de la interface DAO principal arreglado

--- src/dao/gestion/users/models/ModuleDAO.java
+++ src/dao/gestion/users/models/ModuleDAO.java
@@ -1,6 +1,7 @@
 package src.dao.gestion.users.models;
 
 import src.dto.gestion.users.ModuleDTO;
+import src.dao.interfaces.DAO;
 
 public interface ModuleDAO extends DAO<ModuleDTO, Integer>
 {}
--- src/dao/gestion/users/models/ProfileDAO.java
+++ src/dao/gestion/users/models/ProfileDAO.java
@@ -1,6 +1,7 @@
 package src.dao.gestion.users.models;
 
 import src.dto.gestion.users.ProfileDTO;
+import src.dao.interfaces.DAO;
 
 public interface ProfileDAO extends DAO<ProfileDTO, Integer>
 {}
